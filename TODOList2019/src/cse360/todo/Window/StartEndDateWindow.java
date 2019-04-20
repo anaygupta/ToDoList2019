921ba3b7fc84a7f0e838b2d2498361067a9a1d7d
Debashis Biswas <debashis.biswas@asu.edu>
2019-04-20 15:32:46 -0700

    ui adjustments

--- TODOList2019/src/cse360/todo/Window/StartEndDateWindow.java
+++ TODOList2019/src/cse360/todo/Window/StartEndDateWindow.java
@@ -1,6 +1,7 @@
 package cse360.todo.Window;
 
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Toolkit;
@@ -9,6 +10,7 @@ import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 
 import cse360.todo.ListBox.ListBox;
@@ -41,17 +43,19 @@ public class StartEndDateWindow extends JDialog{
 	public JPanel datesPanel() {
 		JPanel datesPanel = new JPanel(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
-		c.anchor = GridBagConstraints.NORTH;
-		c.fill = GridBagConstraints.HORIZONTAL;
+		c.anchor = GridBagConstraints.CENTER;
+		c.fill = GridBagConstraints.BOTH;
 		c.gridwidth = 1;
 		c.gridheight = 1;
 		c.weightx = 1;
 		
+		Font theFont = new Font(Font.DIALOG, Font.BOLD, 18);
 		//0 0
 		c.gridy = 0;
 		c.gridx = 0;
-		
 		JLabel startDate = new JLabel("Start Date: " + box.getSaveData().getStatus().getStartDate().getMonth() + "/" + box.getSaveData().getStatus().getStartDate().getDay() + "/" + box.getSaveData().getStatus().getStartDate().getYear());
+		startDate.setHorizontalAlignment(SwingConstants.CENTER);
+		startDate.setFont(theFont);
 		datesPanel.add(startDate, c);
 		
 		//1 0
@@ -59,8 +63,9 @@ public class StartEndDateWindow extends JDialog{
 		c.gridx = 0;
 		c.weighty = 1.0;
 		JLabel endDate = new JLabel("End Date: " + box.getSaveData().getStatus().getEndDate().getMonth() + "/" + box.getSaveData().getStatus().getEndDate().getDay() + "/" + box.getSaveData().getStatus().getEndDate().getYear());
+		endDate.setHorizontalAlignment(SwingConstants.CENTER);
+		endDate.setFont(theFont);
 		datesPanel.add(endDate, c);
-		
 		
 		return datesPanel;
 	}
